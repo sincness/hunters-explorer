aeb256114cf9e1d194da2c68b359bf43cd4697ff
Malthe <malthe@tedib.dk>
2020-03-11 10:18:13 +0000

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Camera } from '@ionic-native/camera/ngx';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { ModalPage } from '../app/modal/modal.page';
+
+@NgModule({
+  declarations: [AppComponent, ModalPage],
+  entryComponents: [ModalPage],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    IonicStorageModule.forRoot()
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Camera,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent],
+
+})
+export class AppModule {}
---
+++ src/app/map/map.page.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core'
+import { Router } from '@angular/router';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import * as L from 'leaflet';
+import { ToastController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
+
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+})
+export class MapPage{
+
+  map: Map;
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId', {
+      zoomControl: false
+    });
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '',
+    }).addTo(this.map);
+
+    L.control.zoom({
+      position:'topright'
+    }).addTo(this.map);
+
+    var marker = L.marker([28.100555, -15.415743]).addTo(this.map);
+
+    this.map.setView([28.100555, -15.415743], 100);
+
+
+    // marker([28.6, 77]).addTo(this.map)
+    //   .bindPopup('Ionic 4 <br> Leaflet.')
+    //   .openPopup();
+  }
+
+  locatePosition() {
+    this.map.locate({ watch: true }).on("locationfound", (e: any) => {
+      let newMarker = marker([e.latitude, e.longitude], {
+        draggable:false
+      }).addTo(this.map);
+      newMarker.bindPopup('You are located here!').openPopup();
+    });
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+  
+  constructor(private router: Router, public modalController: ModalController, public toastController: ToastController, private menu: MenuController) { }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalPage
+    });
+    return await modal.present();
+  }
+
+  goToMap() {
+    this.router.navigateByUrl('/map2')
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Error',
+      duration: 2000,
+  
+  position: 'top',
+  cssClass: 'toastAfterHeader'
+    });
+    toast.present();
+  }
+
+}
